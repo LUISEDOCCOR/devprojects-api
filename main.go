869f8d129aac67636c93730afda3cb554c702cdc
Luis Eduardo Ocegueda Cortes <luiseduardoocegueda@gmail.com>
2024-06-24 16:14:39 -0600

    feat: create router

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello Word")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		var response = map[string]string{
+			"status": "ok",
+		}
+		json.NewEncoder(w).Encode(response)
+
+	})
+
+	fmt.Println("The server is 🚀")
+	err := http.ListenAndServe(":4000", r)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
